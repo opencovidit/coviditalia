c4413ac171ee9250ffb24163e90d8747c99cf13e
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-05-07 01:27:06 +0200

    Risoluzione problema percorso icone Bootstrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    optimizeDeps: {
+      include: ['chart.js/auto'],
+    },
+    resolve: {
+      alias: {
+        '/bootstrap-italia/dist/svg/sprites.svg/':
+          '../node_modules/bootstrap-italia/dist/svg/sprites.svg/',
+      },
+    },
+  },
+});
