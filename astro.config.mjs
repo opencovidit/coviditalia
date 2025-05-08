94148c3fc304456a377e88f118cc714bb4f21a56
Antonio Fuscaldo <24657656+antoniofuscaldo@users.noreply.github.com>
2025-05-08 03:12:25 +0200

    Aggiornamento configurazione

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://coviditalia.github.io',
-  base: 'coviditalia',
   vite: {
     optimizeDeps: {
       include: ['chart.js/auto'],
